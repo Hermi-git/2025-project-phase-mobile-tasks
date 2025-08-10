9e3b585f64329bcee67179649370ab6aadfb93ea
Hermi-git <andargiehermela1@gmail.com>
2025-08-10 15:58:41 +0300

    refactor: add bloc provider and named route inside main dart

--- mobile/hermela_andargie/lib/main.dart
+++ mobile/hermela_andargie/lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'features/auth/domain/entities/user.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart'; 
+import 'features/auth/presentation/pages/home_page.dart';
+import 'features/auth/presentation/pages/sign_in_page.dart';
+import 'features/auth/presentation/pages/sign_up_page.dart';
+import 'features/auth/presentation/pages/splash_page.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
@@ -12,12 +20,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'E-Commerce App',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: const Scaffold(
-        body: Center(child: Text('App Initialized, build UI next')),
+    return BlocProvider<AuthBloc>(
+      create: (_) => di.sl<AuthBloc>(), // get your bloc instance from DI
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'E-Commerce App',
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        home: const SplashPage(),
+        routes: {
+          '/signin': (context) => const SignInPage(),
+          '/signup': (context) => const SignUpPage(),
+          '/home': (context) {
+            final user = ModalRoute.of(context)!.settings.arguments as User;
+            return HomePage(user: user);
+          },
+        },
       ),
     );
   }
